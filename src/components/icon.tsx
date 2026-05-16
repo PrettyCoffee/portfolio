423003a75165bc61fdd031fb2f05eadfbbcbc438
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-05-16 21:58:56 +0200

    feat: Add icon component

---
+++ src/components/icon.tsx
@@ -0,0 +1,145 @@
+import { ReactNode } from "react"
+
+export interface IconProps {
+  icon: keyof typeof icons
+  color?: string
+  size?: number | string
+  className?: string
+}
+
+const getStyle = (size: number | string) => ({
+  height: size,
+  minHeight: size,
+  width: size,
+  minWidth: size,
+})
+
+export const Icon = ({
+  color = "currentColor",
+  size = "1em",
+  icon,
+  ...rest
+}: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth={2.5}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    style={getStyle(size)}
+    {...rest}
+  >
+    {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define -- moving icons below the component improves the file structure
+      icons[icon]
+    }
+  </svg>
+)
+
+const icons = {
+  "arrow-up": (
+    <>
+      <path d="M5 3h14" />
+      <path d="m18 13-6-6-6 6" />
+      <path d="M12 7v14" />
+    </>
+  ),
+
+  face: (
+    <>
+      <path d="M3 7V5a2 2 0 0 1 2-2h2" />
+      <path d="M17 3h2a2 2 0 0 1 2 2v2" />
+      <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+      <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
+      <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+      <path d="M9 9h.01" />
+      <path d="M15 9h.01" />
+    </>
+  ),
+
+  toolbox: (
+    <>
+      <path d="M10 15h4" />
+      <path d="m14.817 10.995-.971-1.45 1.034-1.232a2 2 0 0 0-2.025-3.238l-1.82.364L9.91 3.885a2 2 0 0 0-3.625.748L6.141 6.55l-1.725.426a2 2 0 0 0-.19 3.756l.657.27" />
+      <path d="m18.822 10.995 2.26-5.38a1 1 0 0 0-.557-1.318L16.954 2.9a1 1 0 0 0-1.281.533l-.924 2.122" />
+      <path d="M4 12.006A1 1 0 0 1 4.994 11H19a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2z" />
+    </>
+  ),
+
+  squiggle: (
+    <path d="M7 3.5c5-2 7 2.5 3 4C1.5 10 2 15 5 16c5 2 9-10 14-7s.5 13.5-4 12c-5-2.5.5-11 6-2" />
+  ),
+
+  hash: (
+    <>
+      <line x1="4" x2="20" y1="9" y2="9" />
+      <line x1="4" x2="20" y1="15" y2="15" />
+      <line x1="10" x2="8" y1="3" y2="21" />
+      <line x1="16" x2="14" y1="3" y2="21" />
+    </>
+  ),
+
+  external: (
+    <>
+      <path d="M15 3h6v6" />
+      <path d="M10 14 21 3" />
+      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+    </>
+  ),
+
+  github: (
+    <>
+      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+      <path d="M9 18c-4.51 2-5-2-7-2" />
+    </>
+  ),
+
+  gitlab: (
+    <path d="m22 13.29-3.33-10a.42.42 0 0 0-.14-.18.38.38 0 0 0-.22-.11.39.39 0 0 0-.23.07.42.42 0 0 0-.14.18l-2.26 6.67H8.32L6.1 3.26a.42.42 0 0 0-.1-.18.38.38 0 0 0-.26-.08.39.39 0 0 0-.23.07.42.42 0 0 0-.14.18L2 13.29a.74.74 0 0 0 .27.83L12 21l9.69-6.88a.71.71 0 0 0 .31-.83Z" />
+  ),
+
+  npm: (
+    <>
+      <path
+        d="M0.9906 7.4962h22.0188v7.0059H10.9991v2.0017H7.4962v-2.0017H0.9906V7.4962Z"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M7.4962 7.4962v7.0059"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M13.5013 7.4962v7.0059"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M18.0051 10.4987v4.0034"
+        strokeLinecap="square"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M4.994 10.4987v4.0034"
+        strokeLinecap="square"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M10.9991 10.4987v1.0009"
+        strokeLinecap="square"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M20.5072 10.4987v4.0034"
+        strokeLinecap="square"
+        strokeWidth="1.5"
+      />
+    </>
+  ),
+} satisfies Record<string, ReactNode>
