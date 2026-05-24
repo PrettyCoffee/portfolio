5120ff2dc88d9bab9a1ff65b4c1c6b5c27d2794a
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-05-24 13:48:45 +0200

    feat(goober): Add theme helper

---
+++ packages/goober/src/create-theme.ts
@@ -0,0 +1,82 @@
+type Indexable = Record<string, unknown>
+type ObjDeepPath<TObj> = TObj extends Indexable
+  ? {
+      [K in keyof TObj]: TObj[K] extends Indexable
+        ? `${Exclude<K, symbol>}.${ObjDeepPath<TObj[K]>}`
+        : K
+    }[keyof TObj]
+  : never
+
+interface TokenItem {
+  [key: string]: TokenItem | string
+}
+
+export type TokenVariants<TVariants extends string = string> = Record<
+  TVariants,
+  TokenItem
+>
+
+const getCssVar = (key: string) => "--" + key.split(".").join("-")
+
+const getDeepKeys = (currentValue: unknown, currentKey = ""): string[] => {
+  if (typeof currentValue === "string") {
+    return [currentKey]
+  }
+  if (currentValue && typeof currentValue === "object") {
+    return Object.entries(currentValue).flatMap(([key, value]) => {
+      const newKey = !currentKey ? key : `${currentKey}.${key}`
+      return getDeepKeys(value, newKey)
+    }, {})
+  }
+  throw new Error(`Value of ${currentKey} could not be processed.`)
+}
+
+export const createTheme = <
+  TSharedTokens extends TokenItem,
+  TVariantTokens extends TokenVariants,
+>(
+  shared: TSharedTokens,
+  variants: TVariantTokens
+) => {
+  type Variant = keyof TVariantTokens
+  type Tokens = TSharedTokens & TVariantTokens[Variant]
+
+  const defaultVariant = Object.keys(variants)[0]!
+  const tokens = Object.fromEntries(
+    Object.entries(variants).map(([key, value]) => [
+      key,
+      { ...shared, ...value },
+    ])
+  ) as Record<Variant, Tokens>
+
+  const getValue = (key: ObjDeepPath<Tokens>, variant: Variant) => {
+    let value: unknown = tokens[variant]
+    for (const segment of key.split(".")) {
+      // @ts-expect-error -- hard to use correct types here
+      value = value[segment]
+    }
+    if (typeof value !== "string") {
+      throw new Error(`Theme key could not be read: ${key}`)
+    }
+    return value.trim()
+  }
+
+  const read = (key: ObjDeepPath<Tokens>) => {
+    const value = getValue(key, defaultVariant)
+    if (Object.keys(shared).includes(key.split(".")[0]!)) {
+      return value
+    }
+    const cssVar = getCssVar(key)
+    return `var(${cssVar}, ${value})`
+  }
+
+  const getCssVars = (variant: Variant) =>
+    Object.fromEntries(
+      getDeepKeys(variants[variant]).map(key => [
+        getCssVar(key),
+        getValue(key as ObjDeepPath<Tokens>, variant),
+      ])
+    )
+
+  return Object.assign(read, { getCssVars, tokens })
+}
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,22 +1,5 @@
-const color = {
-  light: {
-    base: "#D7D3CB",
-    gentle: "#A29E97",
-  },
-  dark: {
-    base: "#282C34",
-    gentle: "#5D6168",
-  },
-  accent: {
-    red: "#E06C75",
-    green: "#98C379",
-    yellow: "#E5C07B",
-    blue: "#61AFEF",
-    purple: "#C678DD",
-    cyan: "#56B6C2",
-    orange: "#FFBB7C",
-  },
-}
+import { createTheme } from "lib/goober"
+
 const breakpoint = {
   1040: "screen and (max-width: 65rem)",
   880: "screen and (max-width: 55rem)",
@@ -60,6 +43,26 @@ const space = {
   x11: "55rem",
 }
 
+const color = {
+  light: {
+    base: "#D7D3CB",
+    gentle: "#A29E97",
+  },
+  dark: {
+    base: "#282C34",
+    gentle: "#5D6168",
+  },
+  accent: {
+    red: "#E06C75",
+    green: "#98C379",
+    yellow: "#E5C07B",
+    blue: "#61AFEF",
+    purple: "#C678DD",
+    cyan: "#56B6C2",
+    orange: "#FFBB7C",
+  },
+}
+
 const boxShadow = (hsl: string) => ({
   low: `
     0px 0.5px 0.5px hsl(${hsl} / 0.91),
@@ -98,9 +101,6 @@ const dropShadow = (hsl: string) => ({
 
 const tokens = {
   dark: {
-    breakpoint,
-    font,
-    space,
     accent: color.accent,
     text: {
       base: color.light.base,
@@ -122,9 +122,6 @@ const tokens = {
     },
   },
   light: {
-    breakpoint,
-    font,
-    space,
     accent: color.accent,
     text: {
       base: color.dark.base,
@@ -147,60 +144,6 @@ const tokens = {
   },
 }
 
-type TokenVariant = keyof typeof tokens
-type Tokens = (typeof tokens)[TokenVariant]
+const shared = { breakpoint, font, space }
 
-type Indexable = Record<string, unknown>
-type ObjDeepPath<TObj> = TObj extends Indexable
-  ? {
-      [K in keyof TObj]: TObj[K] extends Indexable
-        ? `${Exclude<K, symbol>}.${ObjDeepPath<TObj[K]>}`
-        : K
-    }[keyof TObj]
-  : never
-
-const getCssVar = (key: string) => "--" + key.split(".").join("-")
-
-const getValue = (key: ObjDeepPath<Tokens>, variant: TokenVariant) => {
-  let value: unknown = tokens[variant]
-  for (const segment of key.split(".")) {
-    // @ts-expect-error -- hard to use correct types here
-    value = value[segment]
-  }
-  if (typeof value !== "string") {
-    throw new Error(`Theme key could not be read: ${key}`)
-  }
-  return value.trim()
-}
-
-const read = (key: ObjDeepPath<Tokens>) => {
-  const value = getValue(key, "dark")
-  if (/^(breakpoint|font|space)\./.test(key)) return value
-  const cssVar = getCssVar(key)
-  return `var(${cssVar}, ${value})`
-}
-
-const getDeepKeys = (currentValue: unknown, currentKey = ""): string[] => {
-  if (typeof currentValue === "string") {
-    return [currentKey]
-  }
-  if (currentValue && typeof currentValue === "object") {
-    return Object.entries(currentValue).flatMap(([key, value]) => {
-      const newKey = !currentKey ? key : `${currentKey}.${key}`
-      return getDeepKeys(value, newKey)
-    }, {})
-  }
-  throw new Error(`Value of ${currentKey} could not be processed.`)
-}
-const getCssVars = (variant: TokenVariant) => {
-  // eslint-disable-next-line unused-imports/no-unused-vars -- filter space out, since it is not needed as variable
-  const { space, ...scheme } = tokens[variant]
-  return Object.fromEntries(
-    getDeepKeys(scheme).map(key => [
-      getCssVar(key),
-      getValue(key as ObjDeepPath<Tokens>, variant),
-    ])
-  )
-}
-
-export const theme = Object.assign(read, { getCssVars, tokens })
+export const theme = createTheme(shared, tokens)
